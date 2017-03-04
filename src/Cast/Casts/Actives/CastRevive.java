b9bd50e56860426cd1456cac09378f3405997957
Minh Vu <minhd_vu@yahoo.com>
2017-03-03 21:40:32 -0500

    Changed Up Cast Revive (Not Final)
    Untested Skill.

--- src/Cast/Casts/Actives/CastRevive.java
+++ src/Cast/Casts/Actives/CastRevive.java
@@ -17,21 +17,19 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.List;
 
 public class CastRevive extends ActiveCast implements CommandInterface, Listener
 {
-	private HashMap<UUID, Location> deaths;
-
+	private List<Death> deaths;
 	private int range;
 	private int percentage;
-
+	private int timer;
 	public CastRevive(String name, String description)
 	{
 		super(name, description);
 
-		deaths = new HashMap<UUID, Location>();
+		deaths = new ArrayList<Death>();
 
 		warmup.setDuration(0);
 		warmup.setAmplifier(0);
@@ -44,6 +42,7 @@ public class CastRevive extends ActiveCast implements CommandInterface, Listener
 
 		range = 32;
 		percentage = 100;
+		timer = 30000;
 
 		info.add(ChatColor.DARK_AQUA + "Range: " + ChatColor.GRAY + range + " Blocks");
 		info.add(ChatColor.DARK_AQUA + "Percentage: " + ChatColor.GRAY + percentage + "%");
@@ -72,36 +71,48 @@ public class CastRevive extends ActiveCast implements CommandInterface, Listener
 				{
 					Player target = player.getServer().getPlayer(args[1]);
 
-					if (target.isDead())
+					if (caster.canCast(name, cooldown, manacost))
 					{
-						if (caster.canCast(name, cooldown, manacost))
+						warmup.start(caster, target, name);
+
+						new BukkitRunnable()
 						{
-							warmup.start(caster, target, name);
-
-							new BukkitRunnable()
+							@SuppressWarnings("deprecation")
+							@Override
+							public void run()
 							{
-								@SuppressWarnings("deprecation")
-								@Override
-								public void run()
+								for (Death death : deaths)
 								{
-									caster.setCasting(name, true);
-									caster.setMana(manacost);
+									if (death.getPlayer().equals(target))
+									{
+										if (System.currentTimeMillis() - death.getTime() <= timer)
+										{
+											caster.setCasting(name, true);
+											caster.setMana(manacost);
 
-									target.setHealth(target.getMaxHealth() * (percentage / 100));
-									target.getWorld().spigot().playEffect(target.getLocation(), Effect.HEART, 0, 0,
-											0.2F, 0.2F, 0.2F, 0.1F, 50, 16);
-									target.getWorld().playSound(target.getLocation(), Sound.BLOCK_PORTAL_AMBIENT, 8.0F,
-											1.0F);
+											target.setHealth(target.getMaxHealth() * (percentage / 100));
+											target.getWorld().spigot().playEffect(target.getLocation(), Effect.HEART, 0, 0, 0.2F, 0.2F, 0.2F, 0.1F, 50, 16);
+											target.getWorld().playSound(target.getLocation(), Sound.BLOCK_PORTAL_AMBIENT, 8.0F, 1.0F);
 
-									cast(player, target);
+											cast(player, target);
 
-									cooldown.start(player.getName());
+											cooldown.start(player.getName());
 
-									caster.setCasting(name, false);
+											caster.setCasting(name, false);
+										}
+
+										else
+										{
+											caster.getPlayer().sendMessage(header + ChatColor.GRAY + " You Were Too Late. " +
+													ChatColor.WHITE + target.getName() + ChatColor.GRAY + " Could Not Be" + ChatColor.WHITE + " Revived.");
+										}
+
+										break;
+									}
 								}
+							}
 
-							}.runTaskLater(Main.getInstance(), warmup.getDuration());
-						}
+						}.runTaskLater(Main.getInstance(), warmup.getDuration());
 					}
 
 					else
@@ -129,6 +140,35 @@ public class CastRevive extends ActiveCast implements CommandInterface, Listener
 	@EventHandler
 	public void onPlayerDeathEvent(PlayerDeathEvent event)
 	{
-		deaths.put(event.getEntity().getUniqueId(), event.getEntity().getLocation());
+		deaths.add(new Death(event.getEntity(), event.getEntity().getLocation(), System.currentTimeMillis()));
+	}
+
+	private class Death
+	{
+		private Player player;
+		private Location location;
+		private Long time;
+
+		public Death(Player player, Location location, Long time)
+		{
+			this.player = player;
+			this.location = location;
+			this.time = time;
+		}
+
+		public Player getPlayer()
+		{
+			return player;
+		}
+
+		public Location getLocation()
+		{
+			return location;
+		}
+
+		public Long getTime()
+		{
+			return time;
+		}
 	}
 }
